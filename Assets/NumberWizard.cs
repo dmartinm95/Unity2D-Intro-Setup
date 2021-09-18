2f1cde6b0c14a55a497bda12c8f0617afba71201
Diego <dmartinm1995@gmail.com>
2021-09-18 13:46:28 -0700

    Refactored code into functions and game can now be restarted when guess is correct

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -10,6 +10,14 @@ public class NumberWizard : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
+        StartGame();
+    }
+
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
@@ -23,16 +31,19 @@ public class NumberWizard : MonoBehaviour {
     void Update() {
 
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than ... " + guess);
+            NextGuess();
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than ... " + guess);
+            NextGuess();
         } else if (Input.GetKeyDown(KeyCode.Return)) {
             Debug.Log("I'm a genius!");
+            StartGame();
         }
+    }
+
+    void NextGuess() {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than ... " + guess);
     }
 }
