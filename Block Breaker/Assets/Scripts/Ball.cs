bbee6c27c1a58b51182ddd3212f8992a8e35233a
Diego <dmartinm1995@gmail.com>
2021-09-28 12:00:23 -0700

    Can now launch ball from paddle

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     private void Start() {
         paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     private void Update() {
+
+        if (!hasStarted) {
+            LaunchOnMouseClick();
+            LockBallToPaddle();
+        }
+    }
+
+    private void LaunchOnMouseClick() {
+        if (Input.GetMouseButtonDown(0)) {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle() {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
 
         transform.position = paddlePos + paddleToBallVector;
