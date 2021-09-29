1950767888e1770014efff258dd91d8b06ab7b0c
Diego <dmartinm1995@gmail.com>
2021-09-29 14:12:04 -0700

    Added audio implementation when ball bounces

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -7,12 +7,16 @@ public class Ball : MonoBehaviour {
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+
+    AudioSource audioSource;
 
     Vector2 paddleToBallVector;
     bool hasStarted = false;
 
     private void Start() {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update() {
@@ -34,5 +38,12 @@ public class Ball : MonoBehaviour {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
 
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (hasStarted) {
+            AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
+            audioSource.PlayOneShot(clip);
+        }
     }
 }
