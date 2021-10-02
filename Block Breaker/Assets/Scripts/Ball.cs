fdba568097ffec0207dfc3973589938cc4cf1e3e
Diego <dmartinm1995@gmail.com>
2021-10-02 11:46:51 -0700

    Adding randomness to ball bounce to avoid boring loops

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -1,22 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    // Config params
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
     [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
 
+    // Cached references
     AudioSource audioSource;
+    Rigidbody2D rb2d;
 
+    // State variables
     Vector2 paddleToBallVector;
     bool hasStarted = false;
 
     private void Start() {
         paddleToBallVector = transform.position - paddle1.transform.position;
         audioSource = GetComponent<AudioSource>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
@@ -30,7 +34,7 @@ public class Ball : MonoBehaviour {
     private void LaunchOnMouseClick() {
         if (Input.GetMouseButtonDown(0)) {
             hasStarted = true;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            rb2d.velocity = new Vector2(xPush, yPush);
         }
     }
 
@@ -41,9 +45,13 @@ public class Ball : MonoBehaviour {
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
+
+        Vector2 velocityTweak = new Vector2(Random.Range(0f, randomFactor), Random.Range(0f, randomFactor));
+
         if (hasStarted) {
             AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
             audioSource.PlayOneShot(clip);
+            rb2d.velocity += velocityTweak;
         }
     }
 }
