632c4e4b505092c8417baca761f1f31c2483594f
Diego <dmartinm1995@gmail.com>
2021-10-02 11:39:11 -0700

    Added different kinds of blocks

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    // Config params (things we tune)
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
 
+    // Cached references
     Level level;
     GameStatus gameStatus;
+
+    // State variables
+    [SerializeField] int timesHit; // TODO: only serialized for debug purposes
 
     private void Start() {
         CountBreakableBlocks();
@@ -25,7 +31,26 @@ public class Block : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (tag == "Breakable") {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit() {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits) {
             DestroyBlock();
+        } else {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite() {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] != null) {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        } else {
+            Debug.LogError("Block sprite is missing from array - " + gameObject.name);
         }
     }
 
